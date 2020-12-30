63f80205c88ddc3daae1ed172a7c344e02155da1
Jacson Miranda <jacsonmiranda@gmail.com>
2020-12-30 14:23:20 -0300

    js API flag game v2

--- JavaScriptGameFlagWithAPI.js
+++ JavaScriptGameFlagWithAPI.js
@@ -1,36 +1,58 @@
-<html>
+window.onload = init;
+var pick = "";
 
-<head>
-    <title>Complete JavaScript Course</title>
-    <style>
+function init() {
+    document.getElementById('myButton').addEventListener('click', myLoader, false);
+    document.getElementById('myGuess').addEventListener('click', guessWord, false);
+}
 
-    </style>
-</head>
+function myLoader() {
+    var xHR = new XMLHttpRequest();
+    xHR.onload = rOutput;
+    xHR.open('get', 'https://restcountries.eu/rest/v2/all', true);
+    xHR.send();
+}
 
-<body>
-    <div id="output">Complete JavaScript Course </div>
-    <input type="button" id="myButton" value="Load Data">
-    <script>
-        window.onload = init;
-        var pick = "";
-
-        function init() {
-            document.getElementById('myButton').addEventListener('click', myLoader, false);
+function guessWord() {
+    var g = document.getElementById('guess').value;
+    var userGuess = '';
+    g.toLowerCase();
+    for (var x = 0; x < pick.name.length; x++) {
+        if (x >= g.length) {
+            break;
         }
-
-        function myLoader() {
-            var xHR = new XMLHttpRequest();
-            xHR.onload = rOutput;
-            xHR.open('get', 'https://restcountries.eu/rest/v2/all', true);
-            xHR.send();
+        console.log(g[x])
+        console.log(pick.name[x])
+        if (g[x] == pick.name[x].toLowerCase()) {
+            userGuess += g[x];
+            console.log(userGuess);
         }
-
-        function rOutput() {
-            var myObj = JSON.parse(this.responseText);
-            pick = myObj[Math.floor(Math.random() * myObj.length)];
-            console.log(pick);
+        else {
+            userGuess += '-';
         }
-    </script>
-</body>
+    }
+    // toLowerCase
+    document.getElementById('output2').innerHTML = userGuess;
+}
 
-</html>
+function rOutput() {
+    var myObj = JSON.parse(this.responseText);
+    pick = myObj[Math.floor(Math.random() * myObj.length)];
+    var ww = '';
+    for (var x = 0; x < pick.name.length; x++) {
+        ww += '-';
+    }
+    document.querySelector('img').src = pick.flag;
+    document.getElementById('output1').innerHTML = shuffleWord(pick.name);
+    document.getElementById('output2').innerHTML = ww;
+}
+
+function shuffleWord(w) {
+    var response = '';
+    w = w.split('');
+    while (w.length > 0) {
+        response += w.splice(Math.floor(Math.random() * w.length), 1);
+
+    }
+    return response.toLowerCase();
+}
