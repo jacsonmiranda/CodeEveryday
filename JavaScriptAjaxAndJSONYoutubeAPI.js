1acaf8ed32006710b9cbbf0a7392bc0035db6af1
Jacson Miranda <jacsonmiranda@gmail.com>
2021-01-03 20:01:51 -0300

    youtube api search and display itens

--- JavaScriptAjaxAndJSONYoutubeAPI.js
+++ JavaScriptAjaxAndJSONYoutubeAPI.js
@@ -6,10 +6,24 @@ function init() {
 
 function ySearch() {
     var searchTerm = document.getElementById('searchYoutube').value;
-    var url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyB7Q97g-8bR8wp67SoyVJdKyCO0E-KedTg&q=test&maxResults=20';
+    var url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=AIzaSyB7Q97g-8bR8wp67SoyVJdKyCO0E-KedTg&q='+searchTerm;
     getJSON(url, function (data) {
-        console.log(data);
+        showResult(data.items);
     })
+}
+
+function showResult(results){
+    var html = "";
+    for(var value in results){
+        console.log(results[value]);
+        var title = results[value].snippet.title;
+        var description = results[value].snippet.description;
+        var thumbnail = results[value].snippet.thumbnails.default.url;
+        console.log(thumbnail);
+        html = html + '<div><h3>'+title+'</h3><p>'+description+'</p><img src="'+thumbnail+'"></div>';
+    
+    }
+    document.getElementById('output1').innerHTML = html;
 }
 
 function getJSON(url, callback) {
